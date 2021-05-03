a0c361fc71fad443340daedefa440e8870a24e26
pinnop <77680108+pinnop@users.noreply.github.com>
2021-05-03 17:13:22 +0700

    Add files via upload

---
+++ InsertProductcolor.sql
@@ -0,0 +1,58 @@
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110001', 10001, 'C0011');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110002', 10001, 'C0012');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110003', 10001, 'C0010');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110004', 10002, 'C0011');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110005', 10002, 'C0010');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110006', 10003, 'C0002');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110007', 10003, 'C0005');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110008', 10003, 'C0011');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110009', 10003, 'C0010');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110010', 10004, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110011', 10004, 'C0004');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110012', 10004, 'C0007');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110013', 10005, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110014', 10005, 'C0002');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110015', 10006, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110016', 10006, 'C0002');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110017', 10007, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110018', 10007, 'C0002');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110019', 10007, 'C0001');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110020', 10008, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110021', 10008, 'C0010');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110022', 10009, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110023', 10009, 'C0002');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110024', 10009, 'C0001');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110025', 10009, 'C0007');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110026', 10010, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110027', 10010, 'C0012');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110028', 10011, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110029', 10011, 'C0012');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110030', 10011, 'C0001');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110031', 10011, 'C0007');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110032', 10012, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110033', 10012, 'C0005');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110034', 10012, 'C0001');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110035', 10013, 'C0011');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110036', 10014, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110037', 10014, 'C0001');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110038', 10014, 'C0007');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110039', 10015, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110040', 10015, 'C0002');
+
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110041', 10016, 'C0006');
+INSERT INTO productcolor(productcolorid, productid, colorid) VALUES('110042', 10016, 'C0002');
+
